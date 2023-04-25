d778d9ea335a938ef76c024e6dbe64e7c970cf54
Moran, Lucas <lucas.moran40@gmail.com>
2023-04-25 15:39:46 -0400

    added missing plugins after loading

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,37 @@
+local lsp = require('lsp-zero')
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'rust_analyzer'
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-J>"] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+    sign_icons = { }
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+  lsp.buffer_autoformat()
+end)
+
+lsp.format_on_save({
+  servers = {
+    ['lua_ls'] = {'lua'},
+    ['rust_analyzer'] = {'rust'},
+    ['tsserver'] = {'js', 'ts', 'tsx', 'jsx'}
+  }
+})
+
+lsp.setup()
