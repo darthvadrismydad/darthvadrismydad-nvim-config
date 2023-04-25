80344356105a1585e11026462ea634c809635e69
Moran, Lucas <lucas.moran40@gmail.com>
2023-04-25 15:34:52 -0400

    initial commit

---
+++ lua/plug.lua
@@ -0,0 +1,40 @@
+return require('packer').startup({function(use)
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+   config = {
+        package_root = vim.fn.stdpath('config') .. '/site/pack'
+    }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }) 
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use 'mbbill/undotree'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }} 
+    end
+})
